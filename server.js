83da507f55ffe7a71279808b061fd10330f0faaa
Agustin Jalil Leon <115709245+agustin-jalil@users.noreply.github.com>
2025-05-16 09:33:45 -0300

    Update server.js cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Permitir solicitudes desde Vite (frontend)
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
