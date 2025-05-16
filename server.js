98c809397ebb4b9f4f26959ae98dd9f14a668a3f
Agustin Jalil Leon <agustinjalilleon@gmail.com>
2025-05-16 18:01:25 -0300

    cors sin barra

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Permitir solicitudes desde Vite (frontend)
 app.use(cors({
-  origin: 'https://diplomatura-sprint-6-agustinjalil.netlify.app/',
+  origin: 'https://diplomatura-sprint-6-agustinjalil.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
